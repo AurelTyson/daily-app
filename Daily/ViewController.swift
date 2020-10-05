98b180fd211ca829514035e4cb1a1c7118b07771
Aurel Tyson <aurel.tis@gmail.com>
2020-10-05 20:53:28 +0200

    Init project

---
+++ Daily/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Daily
+//
+//  Created by Aurélien Tison on 05/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
